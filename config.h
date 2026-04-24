9b3afc2d49d1c010bc46614a6db6e17622e7e8ee
Sam Shaaban <samueljshaaban@gmail.com>
2026-04-24 12:01:52 -0700

    initial draft of config file, moving pin_definitions here

---
+++ config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+enum Bot {A, B};
+
+class Config {
+  public:
+  Bot get_bot();
+
+  class pins {
+  public:
+  static const int =
+    Lidar_Ready = 9,
+    Lidar_SDA = 46;
+  };
+
+}
--- pin_definitions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Pins {
-  public:
-  const int
-    Lidar_Ready = 9,
-    Lidar_SCL = 46;
-};
