b8bc338aec4a68ab736a838cb4ece88f70eb1a1f
Sam Shaaban <samueljshaaban@gmail.com>
2026-05-04 23:08:21 -0700

    some more comments on an alternative approach to exporting control signals

--- controller.h
+++ controller.h
@@ -14,21 +14,40 @@ perhaps change what is on the gauges / display?
 */
 struct Keys {
   public:
-  double drive_x, drive_y;
+  double drive_x, drive_y; // drive motors - 1 stick
+  double aux1, aux2, aux3, aux4; // mappable remaining analog inputs
 
-  // auxiliary motors, but these depend on configuration so not sure
-
+  unsigned int motor_cmd; // number to run an auxiliary motor routine
   unsigned int media_cmd; // probably enumerate configured files to play and just send a number to media, 0 = nothing
-  unsigned double volume; // only for A?
 
   // maybe some other cmd to control gauges or something
 };
+
+// then each controllable module reads config to map aux analog and cmds to specific behavior
 
 
 class Controller {
   public:
   Controller(ControllerType);
   void setup();
-  Keys read(); // or we could store keys privately, and add various functions to read different things
   void set_battery(double);
+
+  Keys read(); // or we could store keys privately, and add various functions to read different things
+
+  /*
+  as an alternative to the keys struct, we could make a few different functions
+
+  struct Pair { double x, y; }; 
+  Pair get_drive();
+  Pair get_aux_stick();
+  double get_trigger(int num); // unsigned values unlike stick
+
+  and maybe have cmds retrieved from a queue, but probably best to separate to destinations here so multiple different modules can pop from same Controller object easily
+  unsigned int get_motor_cmd(); // pop and return first element of queue
+  unsigned int get_media_cmd();
+  unsigned int get_other_cmd();
+
+  private:
+  Queue motor_cmds, media_cmds;
+  */
 };
